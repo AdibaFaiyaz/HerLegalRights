d02d7a56d6a59e41f808423a99f7d622dd8ba3c6
adibafaiyaz16@gmail.com <adibafaiyaz16@gmail.com>
2024-03-10 17:14:51 +0530

    Navbar - hover effect

--- Frontend/src/Components/Nav/Nav.js
+++ Frontend/src/Components/Nav/Nav.js
@@ -7,36 +7,36 @@ function Nav() {
     <div className="  bg-gray-50">
       <div className="mx-2 flex justify-between">
         <Link to="/" class="navbar-brand">
-          <img src={Logo} className="h-28 mx-4" />
+          <img src={Logo} className="h-20 mx-4" />
         </Link>
         <div className="font-medium flex flex-row space-x-8 rtl:space-x-reverse my-auto justify-end mx-10 px-10">
           <Link
             to="/Resources"
-            className="block py-2 px-3 text-black bg-blue-700 rounded md:bg-transparent  md:p-0 dark:text-black "
+            className="text-black  hover:bg-red-400 focus:outline-none font-lg rounded-lg text-sm px-5 py-2.5  dark:hover:bg-red-400"
           >
             Legal Resources
           </Link>
           <Link
             to="/Case"
-            className="block py-2 px-3 text-black bg-blue-700 rounded md:bg-transparent  md:p-0 dark:text-black "
+            className="text-black  hover:bg-red-400 focus:outline-none font-lg rounded-lg text-sm px-5 py-2.5  dark:hover:bg-red-400"
           >
             Case Studies
           </Link>
           <Link
             to="/Workshop"
-            className="block py-2 px-3 text-black bg-blue-700 rounded md:bg-transparent  md:p-0 dark:text-black "
+            className="text-black  hover:bg-red-400 focus:outline-none font-lg rounded-lg text-sm px-5 py-2.5  dark:hover:bg-red-400"
           >
             Empowerment Workshop
           </Link>
           <Link
             to="/Actions"
-            className="block py-2 px-3 text-black bg-blue-700 rounded md:bg-transparent  md:p-0 dark:text-black "
+            className="text-black  hover:bg-red-400 focus:outline-none font-lg rounded-lg text-sm px-5 py-2.5  dark:hover:bg-red-400"
           >
             Legal Laws
           </Link>
           <Link
             to="/About"
-            className="block py-2 px-3 text-black bg-blue-700 rounded md:bg-transparent  md:p-0 dark:text-black "
+            className="text-black  hover:bg-red-400 focus:outline-none font-lg rounded-lg text-sm px-5 py-2.5  dark:hover:bg-red-400"
           >
             About us
           </Link>
